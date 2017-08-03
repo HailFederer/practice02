8492efaa370552b165dbd25e15959401f7d648e8
Alex <urimaumdero@hotmail.com>
2017-08-03 17:18:01 +0900

    prob5 - 캡슐화 및 else if문 명료화

--- src/prob5/Prob5.java
+++ src/prob5/Prob5.java
@@ -4,64 +4,59 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Prob5 {
+	
+	public static void randomGame() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// 게임 변수 초기화
+		int minNumber = 1;
+		int maxNumber = 100;
+
+		// 정답 램덤하게 만들기
+		Random random = new Random();
+		int correctNumber = random.nextInt(maxNumber) + minNumber;
+		
+		int tryNum;
+		int repeatNum=1;
+		
+		System.out.println(correctNumber);
+		
+		System.out.println("수를 결정하였습니다. 맞춰보세요.");
+		while(true){
+			System.out.println(minNumber+"-"+maxNumber);
+			System.out.print(repeatNum+"번째 시도>> ");
+			
+			tryNum = sc.nextInt();
+			
+			if(correctNumber == tryNum){
+				System.out.println("정답입니다!");
+				break;
+			}else if(tryNum<minNumber || tryNum>maxNumber) {
+				System.out.println("범위 내의 값으로 다시 입력해주세요.");
+				continue;					
+			}else if(correctNumber < tryNum){
+					System.out.println("더 낮게");
+					maxNumber = tryNum-1;
+			}else if(correctNumber > tryNum){
+					System.out.println("더 높게");
+					minNumber = tryNum+1;
+			}
+			repeatNum++;
+		}		
+	}
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Random random = new Random();
 		
-		String answer = null;
-		do {
-			// 게임 변수 초기화
-			int minNumber = 1;
-			int maxNumber = 100;
-
-			// 정답 램덤하게 만들기
-			int correctNumber = random.nextInt(maxNumber) + minNumber;
-			
-			int tryNum;
-			int repeatNum=1;
-			
-			System.out.println(correctNumber);
-			
-			System.out.println("숫자를 맞춰보세요.(1~100)");
-			while(true){
-				
-				System.out.print(repeatNum+"번째 시도>> ");
-				
-				boolean valid = false;
-				do {
-					tryNum = sc.nextInt();
-					
-					if(tryNum<minNumber || tryNum>maxNumber){
-						System.out.println("범위 내의 값으로 다시 입력해주세요.");
-					}else{
-						valid =true;
-					}
-				} while (valid==false);
-				
-				if(correctNumber == tryNum){
-					System.out.println("정답입니다!");
-					break;
-				}else{
-					if(correctNumber < tryNum){
-						System.out.println("더 낮게");
-						maxNumber = tryNum-1;
-					}else{
-						System.out.println("더 높게");
-						minNumber = tryNum+1;
-					}
-					
-					System.out.println(minNumber+"~"+maxNumber);
-				}
-				
-				repeatNum++;
-				
-			}
-			
+		while(true) {
+			randomGame();
 			System.out.print("다시 하시겠습니까?(y/n)");
-			answer = sc.next();
-			
-		} while (answer.equals("y") || answer.equals("Y"));
+			if(sc.next().equals("n")) {
+				break;
+			}
+		}
+		sc.close();
 	}
 }
